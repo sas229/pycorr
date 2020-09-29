6e4feef24cde226d09e5b70760d390372baf9aef
Claire <lclaire@ethz.ch>
2020-09-29 12:03:36 +0100

    new function reliability_guided

--- pycorr/mesh.py
+++ pycorr/mesh.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .subset import*
 
 def neighbours(i, j, subsets, solved, preconditioned, roi, iterations, zncc, p_precond, tol, max_diffnorm, max_iterations):
     n_vec = np.asarray(([-1,-1],[-1,0], [-1, 1],[0,-1],[0,1],[1,-1],[1,0],[1,1]), dtype=int)
@@ -42,3 +43,61 @@ def neighbours(i, j, subsets, solved, preconditioned, roi, iterations, zncc, p_p
                     else:
                         print('Cannot solve this subset...')
     return solved, preconditioned, roi, iterations, zncc
+
+
+
+def reliability_guided(x_m, y_m, ref, tar, template, max_diffnorm, max_iterations, p, tol):
+    roi = np.ones(x_m.shape, dtype=bool)
+    preconditioned = np.zeros(x_m.shape, dtype=bool)
+    solved = np.zeros(x_m.shape, dtype=int)
+    seed_index = np.asarray((3,3), dtype=int)
+    iterations = np.zeros(x_m.shape, dtype=int)
+    zncc = np.zeros(x_m.shape, dtype=np.float64)
+    SSSIG = np.zeros(x_m.shape, dtype=np.float64)
+    stddev_intensity = np.zeros(x_m.shape, dtype=np.float64)
+    
+    # Create subsets.
+    subsets = np.empty((x_m.shape), dtype=object)
+    for i in range(x_m.shape[0]):
+        for j in range(x_m.shape[1]):
+            x_s = x_m[i,j]
+            y_s = y_m[i,j]
+            subsets[i,j] = Subset(x_s, y_s, ref, tar, template)
+    
+    # Solve seed subset.
+
+    subsets[seed_index[0],seed_index[1]].solve(max_diffnorm, max_iterations, p)
+    solved[seed_index[0],seed_index[1]] = -1 # -1 indicates that the subset has been solved but is not currently in the queue.
+    iterations[seed_index[0],seed_index[1]] = subsets[seed_index[0],seed_index[1]].iterations
+    preconditioned[seed_index[0],seed_index[1]] = False
+    zncc[seed_index[0],seed_index[1]] = subsets[seed_index[0],seed_index[1]].zncc
+    
+    # Solve neighbours around seed.
+    p_precond = subsets[seed_index[0],seed_index[1]].p
+    i = seed_index[0]
+    j = seed_index[1]
+    solved, preconditioned, roi, iterations, zncc = neighbours(i, j, subsets, solved, preconditioned, roi, iterations, zncc, p_precond, tol, max_diffnorm, max_iterations)
+    
+    # Work through sorted queue until empty, adding neighbours recursively.
+    while np.max(solved) > -1:
+        # Identify next subset to solve neighbours at.
+        queue = solved*zncc
+        # print(len(np.where(queue>0)[0]))
+        i, j = np.unravel_index(queue.argmax(), queue.shape)
+        p_precond = subsets[i,j].p
+        
+        # Remove current subset from queue.
+        solved[i,j] = -1
+        
+        # Solve neighbours for current subset.
+        solved, preconditioned, roi, iterations, zncc = neighbours(i, j, subsets, solved, preconditioned, roi, iterations, zncc, p_precond, tol, max_diffnorm, max_iterations)
+    
+    x = np.vectorize(lambda subset: subset.coord[0])(subsets).flatten()
+    y = np.vectorize(lambda subset: subset.coord[1])(subsets).flatten()
+    u = np.vectorize(lambda subset: subset.u)(subsets).flatten()
+    v = np.vectorize(lambda subset: subset.v)(subsets).flatten()
+    SSSIG_hist = np.vectorize(lambda subset: subset.SSSIG)(subsets).flatten()
+    sigma_intensity_hist = np.vectorize(lambda subset: subset.sigma_intensity)(subsets).flatten()
+    zncc_hist = np.vectorize(lambda subset: subset.zncc)(subsets).flatten()
+    iterations_hist = np.vectorize(lambda subset: subset.iterations)(subsets).flatten()
+    return x, y, u, v, SSSIG_hist, sigma_intensity_hist, zncc_hist, iterations_hist
